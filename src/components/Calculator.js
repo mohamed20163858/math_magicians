42dfaccb6e7b63f0619eaf789f035b83a7745857
Mohamed Saleh <mohamed20163858@gmail.com>
2022-09-22 01:38:42 +0200

    disable linter to solve PROP-TYPES SHOULD BE LISTED IN THE PROJECT DEPENDENCIES problem

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
 import './Calculator.css';
